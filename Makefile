7f908dffd08e2de65c505310b1c023120b0e92b4
ncasler <ncasler@illinois.edu>
2015-11-20 11:04:01 -0600

    Added reprojection function

--- Makefile
+++ Makefile
@@ -2,17 +2,19 @@
 HDF_INSTALL = /sw/hdf5-1.8.15-para
 MPI_INSTALL = /sw/EasyBuild/software/MPICH/3.1.4-GCC-4.9.2-binutils-2.25
 LIBLAS_INSTALL = /gpfs_scratch/ncasler/liblas
+PROJ_INSTALL=/gpfs_scratch/ncasler/proj
 #SZIP_INSTALL = /sw/EasyBuild/software/Szip/2.1-gmpolf-2015
-EXTLIB      = -L$(HDF_INSTALL)/lib -L$(MPI_INSTALL)/lib -L$(LIBLAS_INSTALL)/lib
+EXTLIB      = -L$(HDF_INSTALL)/lib -L$(MPI_INSTALL)/lib -L$(LIBLAS_INSTALL)/lib -L$(PROJ_INSTALL)/lib
 CC          = mpicc
 CFLAGS      =
-LIB         =  -lz -lm -lrt -lmpich -lpthread -ldl -llas -llas_c
+LIB         =  -lz -lm -lrt -lmpich -lpthread -ldl -llas -llas_c -lproj
 
-INCLUDE     = -I$(HDF_INSTALL)/include -I$(MPI_INSTALL)/include -I$(LIBLAS_INSTALL)/include
+INCLUDE     = -I$(HDF_INSTALL)/include -I$(MPI_INSTALL)/include -I$(LIBLAS_INSTALL)/include -I$(PROJ_INSTALL)/include
 #-I$(SZIP_INSTALL)/include
 
 LIBSHDF     = $(EXTLIB) $(HDF_INSTALL)/lib/libhdf5.a $(MPI_INSTALL)/lib/libmpi.a 
-LIBSLAS      = $(EXTLIB) $(LIBLAS_INSTALL)/lib/liblas_c.so $(LIBLAS_INSTALL)/lib/liblas_c.so.3
+LIBSLAS     = $(EXTLIB) $(LIBLAS_INSTALL)/lib/liblas_c.so $(LIBLAS_INSTALL)/lib/liblas_c.so.3
+LIBSPROJ    = $(EXTLIB) $(PROJ_INSTALL)/lib/libproj.a
 #$(SZIP_INSTALL)/lib/libsz.a
 
 all: h5_crtdat \
@@ -34,7 +36,7 @@ h5_writechunk: h5_writechunk.c
 	$(CC) $(CFLAGS) -o $@ h5_writechunk.c $(INCLUDE) $(LIBSHDF) $(LIB)
 
 readLas: readLas.c
-	$(CC) $(CFLAGS) -o $@ readLas.c $(INCLUDE) $(LIBSLAS) $(LIB)
+	$(CC) $(CFLAGS) -o $@ readLas.c $(INCLUDE) $(LIBSLAS) $(LIBSPROJ) $(LIB)
 
 clean: 
 	rm -f *.h5 *.o \
--- readLas.c
+++ readLas.c
@@ -8,8 +8,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include <liblas/capi/liblas.h>
+#include <proj_api.h>
 
 //LASPointSummary* SummarizePoints(LASReaderH reader);
 //void print_point_summary(FILE *file, LASPointSummary* summary LASHeaderH header);
@@ -50,6 +52,53 @@ void ptime(const char *const msg)
     fprintf(stderr, "cumulative CPU time thru %s = %f\n", msg, t);
 }
 
+
+projPJ loadProj(const char* proj4string)
+{
+    projPJ pj;
+    pj = pj_init_plus(proj4string);
+    //printf("error-state: %s\n", pj_strerrno(*pj_get_errno_ref()));
+    if (!pj)
+    {
+        fprintf(stderr, "Error: Failed to initialize WGS84 projection\n");
+        exit(1);
+    } else {
+        printf("LOADED: Projection: %s\n", pj_get_def(pj, 0));
+        return pj;
+    }
+    
+}
+
+projPJ getProj(LASHeaderH header) 
+{
+    LASSRSH srs = LASHeader_GetSRS(header);
+    char* projStr = LASSRS_GetProj4(srs);
+    printf("Projection is: %s\n", projStr);
+    projPJ pj = loadProj(projStr);
+
+    return pj;
+    
+}
+
+int project(projPJ pj_src, projPJ pj_wgs, double x, double y, double z)
+{
+    printf("SourceProj: %s\n", pj_get_def(pj_src, 0));
+    printf("DestProj: %s\n", pj_get_def(pj_wgs, 0));
+    if (!(pj_src || pj_wgs)) {
+        fprintf(stderr, "Error: projections not initialized.\n");
+        exit(1);
+    }
+
+    pj_transform(pj_src, pj_wgs, 1, 1, &x, &y, &z);
+    return 0;
+}
+
+
+
+
+
+
+
 int main (int argc, char* argv[])
 {
     int i;
@@ -59,9 +108,8 @@ int main (int argc, char* argv[])
     LASReaderH reader = NULL;
     LASHeaderH header = NULL;
     LASPointH p = NULL;
-    double px, py, pz;
-
-
+    //double x, y, z;
+    projPJ pj_src, pj_wgs;
 
 
     for (i = 1; i < argc; i++) {
@@ -126,24 +174,58 @@ int main (int argc, char* argv[])
         fprintf(stderr, "first pass reading %d points ...\n", LASHeader_GetPointRecordsCount(header));
     }
 
-    p = LASReader_GetNextPoint(reader);
+    int pntCount = LASHeader_GetPointRecordsCount(header);
+    double *x,*y,*z;
+    x = malloc(sizeof(double) * pntCount);
+    y = malloc(sizeof(double) * pntCount);
+    z = malloc(sizeof(double) * pntCount);
+    
+
+    fprintf(stderr, "first pass reading %d points ...\n", pntCount);
+    printf("Checking Source projection:\n");
+    pj_src = getProj(header);
+    pj_wgs = loadProj("+proj=longlat +ellps=WGS84 +datum=WGS84 +vunits=m +no_defs");
+
+    /*p = LASReader_GetNextPoint(reader);
     if (!p) {
         if (LASError_GetLastErrorNum())
             LASError_Print("Not able to fetch a point. LASReader is invalid");
         else
             LASError_Print("File does not contain any points to read.");
         exit(1);
+    }*/
+    /** Should refactor this to allow for mpi integration, offset and end can be used to separate processes**/
+    int poff = 0;
+    i = 0;
+    p = LASReader_GetPointAt(reader, i);
+    while ((poff + i) < (pntCount +1)) {
+        x[i] = LASPoint_GetX(p) * LASHeader_GetScaleX(header) + LASHeader_GetOffsetX(header);
+        y[i] = LASPoint_GetY(p) * LASHeader_GetScaleY(header) + LASHeader_GetOffsetY(header);
+        z[i] = LASPoint_GetZ(p) * LASHeader_GetScaleZ(header) + LASHeader_GetOffsetZ(header);
+        p = LASReader_GetNextPoint(reader);
+        //fprintf(stderr,"Loading point %d: %9.6f, %9.6f, %9.6f\n",i, x[i], y[i], z[i]);
+        i++;
     }
+
+    printf("%9.6f, %9.6f, %9.6f\n", x[0], y[0], z[0]);
+    printf("Projecting Point\n");
     
-    px = LASPoint_GetX(p) * LASHeader_GetScaleX(header) + LASHeader_GetOffsetX(header);
-    py = LASPoint_GetY(p) * LASHeader_GetScaleY(header) + LASHeader_GetOffsetY(header);
-    pz = LASPoint_GetZ(p) * LASHeader_GetScaleZ(header) + LASHeader_GetOffsetZ(header);
-    
-    printf("%9.f, %9.f, %9.f\n", px, py, pz);
+    //project(pj_src, pj_wgs, x, y, z);
+    pj_transform(pj_src, pj_wgs, pntCount, 1, &x[0], &y[0], &z[0]);
+    for (i = 0; i < pntCount; i++) {
+        x[i] *= RAD_TO_DEG;
+        y[i] *= RAD_TO_DEG;
+    }
+    printf("%9.6f, %9.6f, %9.6f\n", x[0], y[0], z[0]);
     printf("Successfully opened the %s\n", file_name_in);
 
 
     /* Clean up */
+    pj_free(pj_src);
+    pj_free(pj_wgs);
+    free(x);
+    free(y);
+    free(z);
     LASReader_Destroy(reader);
     reader = NULL;
     LASHeader_Destroy(header);
