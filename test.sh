4bdb1371fac228aa792694c2f277e0ccc3305704
ncasler <ncasler@illinois.edu>
2016-02-23 17:08:35 -0600

    Updated directory structure

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-module load zlib
-module load hdf5-parallel
-
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/gpfs_scratch/ncasler/liblas/lib:/gpfs_scratch/ncasler/proj/lib"
-
-gdb --args mpirun -np 2 ./readLas -i /projects/isgs/lidar/dupage/las/POINTS_001.las
